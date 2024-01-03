ce5254d5a307621b12dd63b1bfb240905cf8e0aa
rgauna79 <rgauna@gmail.com>
2024-01-02 20:44:15 -0700

    convertHandler done

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,40 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    console.log(initNum, initUnit);
+    if (initNum === "invalid number" && initUnit === "invalid unit") {
+      res.json("invalid number and unit");
+    } else if (initNum === "invalid number") {
+      res.json("invalid number");
+    } else if (initUnit === "invalid unit") {
+      res.json("invalid unit");
+    } else {
+      let returnUnit = convertHandler.getReturnUnit(initUnit);
+      let returnNum = convertHandler.convert(initNum, initUnit);
+      let toString =  convertHandler.getString(
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit
+      )
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string: toString,
+      })
+    }
+
+
+  });
 };
